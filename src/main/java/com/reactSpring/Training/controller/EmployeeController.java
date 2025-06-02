61f4e4fea840a9670303cb41a15cbbd9a161d658
fullstack <fullstacktraining25@gmail.com>
2025-06-02 15:30:49 +0530

    updated

--- src/main/java/com/reactSpring/Training/controller/EmployeeController.java
+++ src/main/java/com/reactSpring/Training/controller/EmployeeController.java
@@ -40,7 +40,7 @@ public class EmployeeController {
         return ResponseEntity.ok(employees);
     }
 
-    // 🔹 Update Employee
+    // 🔹 Update Employee details
     @PutMapping("/{id}")
     public ResponseEntity<EmployeeDto> updateEmployee(@PathVariable("id") Long employeeId, @Valid @RequestBody EmployeeDto updatedEmployee) {
         EmployeeDto employeeDto = employeeService.updateEmployee(employeeId, updatedEmployee);
